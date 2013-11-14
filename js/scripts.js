9e3fb5fdcb27df6b446e605d64ab5f1668735825
Joseph Bergdoll <j@varsity.is>
2013-11-14 00:00:30 -0500

    pre-css media query cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,16 @@
 $(document).ready(function(){
-	$('.hamburger').click(function(){
-		$('#sections').addClass('showSections');
-		$('.wrapper').addClass('slideWrapper').delay(400).queue(function(next){
-			$(this).addClass('fade');
-			next();
-			});
-			
-		$('#header').addClass('slideWrapper').delay(400).queue(function(next){
-    $(this).addClass('fade');
-    next();
-});
-	});
-	
-	$('#sections a').click(function(){
-		$('#sections').removeClass('showSections');
-		$('.wrapper').removeClass('fade').delay(100).queue(function(next){
-			$(this).removeClass('slideWrapper');
-			next();
-			});
-			
-		$('#header').removeClass('fade').delay(100).queue(function(next){
-    $(this).removeClass('slideWrapper');
-    next();
-});
-});
+
+
+$('.hero').fitText(2.2, { minFontSize: '23px', maxFontSize: '50px' });
+
+
 	
 	$(".scroll").click(function(event){	
 event.preventDefault();
-$('html,body').animate({scrollTop:$(this.hash).offset().top - $('header').outerHeight()}, 750);
-});
-
-$(".touchScroll").click(function(event){	
-event.preventDefault();
-$('html,body').delay(300).animate({scrollTop:$(this.hash).offset().top - $('header').outerHeight()}, 1000);
+$('html,body').animate({scrollTop:$(this.hash).offset().top - $('header').outerHeight()}, 550);
 });
 	
 	
-	
-	
-	$('div.close').click(function(){
-		$('#sections').removeClass('showSections');
-		$('.wrapper').removeClass('fade').delay(400).queue(function(next){
-    $(this).removeClass('slideWrapper');
-    next();
-    });
-		$('#header').removeClass('fade').delay(400).queue(function(next){
-    $(this).removeClass('slideWrapper');
-    next();
-    });
-	});
-	
-	$('h1.hero').fitText(2.2, { minFontSize: '23px', maxFontSize: '50px' }).addClass('left').delay(1200).queue(function(next){
-	$(this).removeClass('left');
-	next();
-	});
-	
-	$('.one').delay(500).queue(function(next){
-	$(this).addClass('newbg');
-	next();
-	});
 
 	
 
